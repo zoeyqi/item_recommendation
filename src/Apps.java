8a78a1749a1950fbd05580c7607b1f662b4b0a85
xy61@duke.edu <xy61@duke.edu>
2016-02-23 01:09:44 -0500

    input from terminal

--- src/Apps.java
+++ src/Apps.java
@@ -1,17 +1,48 @@
 import com.ECS.client.jax.Item;
+import java.util.Scanner;
 import com.ECS.client.jax.ItemSearchResponse;
 import com.ECS.client.jax.Items;
 
 public class Apps {
+	private static Scanner scanner;
+
 	/*
 	 * The actual application that performs API search
 	 * and displays a returned list of items
 	 */
 	
 	public static void main (String[] args) {
-		// This demonstrates how to use AWSRequest class
+		
+		/*
+		 * get input from terminal
+		 */
+		
+		scanner = new Scanner(System.in);
+
+		//  prompt asking for search index
+		System.out.print("please chosse your search index from \n"+" Books, DVD, Music, Apparel, Video, Jewelry, Automotive, Watch, Electronics\n");
+
+	    System.out.print("Enter your search index: ");
+
+	    // get their input as a String
+		String searchIndex = scanner.nextLine();//suggest: Books
+		    
+		// prompt asking for search key
+		System.out.print("Enter your search Key: ");//suggest: software engineering 
+
+	    // get the input as an string
+		String searchKey = scanner.nextLine();
+
+		System.out.println(String.format("your are seaching for %s, under %s", searchKey, searchIndex));
+			
+		System.out.println("==================");	
+		
+		/*
+		 *  This demonstrates how to use AWSRequest class
+		 */
+		
 		AWSRequest requestObj = new AWSRequest();
-		requestObj.setRequest("Books", "software engineering", "ItemAttributes,Reviews");
+		requestObj.setRequest(searchIndex, searchKey, "ItemAttributes,Reviews");
 		ItemSearchResponse result = requestObj.getResponse();
 		// For all the items returned in the response, get all the title names
 		for (Items itemList : result.getItems()) {
